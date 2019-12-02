66027e9f6bb91cabe7aedc57908bbda28dd65575
N <plhammerdev@gmail.com>
2019-12-02 10:48:10 -0500

    Merge of session and request headers

--- request_wrapper.py
+++ request_wrapper.py
@@ -1,7 +1,9 @@
 import logging
 import pycurl
 import re
-from collections.abc import Mapping
+from collections.abc import Mapping, MutableMapping
+
+from collections import OrderedDict
 from urllib.parse import urlencode, urljoin, urlsplit
 from response import Response
 from io import BytesIO
@@ -40,6 +42,77 @@ def resp_header_parse(header_line):
     # Now we can actually record the header name and value.
     # Note: this only works when headers are not duplicated, see below.
     _resp_headers[name] = value
+
+
+class CaseInsensitiveDict(MutableMapping):
+    """
+    https://github.com/psf/requests/blob/eedd67462819f8dbf8c1c32e77f9070606605231/requests/structures.py#L15
+    A case-insensitive ``dict``-like object.
+    Implements all methods and operations of
+    ``MutableMapping`` as well as dict's ``copy``. Also
+    provides ``lower_items``.
+    All keys are expected to be strings. The structure remembers the
+    case of the last key to be set, and ``iter(instance)``,
+    ``keys()``, ``items()``, ``iterkeys()``, and ``iteritems()``
+    will contain case-sensitive keys. However, querying and contains
+    testing is case insensitive::
+        cid = CaseInsensitiveDict()
+        cid['Accept'] = 'application/json'
+        cid['aCCEPT'] == 'application/json'  # True
+        list(cid) == ['Accept']  # True
+    For example, ``headers['content-encoding']`` will return the
+    value of a ``'Content-Encoding'`` response header, regardless
+    of how the header name was originally stored.
+    If the constructor, ``.update``, or equality comparison
+    operations are given keys that have equal ``.lower()``s, the
+    behavior is undefined.
+    """
+
+    def __init__(self, data=None, **kwargs):
+        self._store = OrderedDict()
+        if data is None:
+            data = {}
+        self.update(data, **kwargs)
+
+    def __setitem__(self, key, value):
+        # Use the lowercased key for lookups, but store the actual
+        # key alongside the value.
+        self._store[key.lower()] = (key, value)
+
+    def __getitem__(self, key):
+        return self._store[key.lower()][1]
+
+    def __delitem__(self, key):
+        del self._store[key.lower()]
+
+    def __iter__(self):
+        return (casedkey for casedkey, mappedvalue in self._store.values())
+
+    def __len__(self):
+        return len(self._store)
+
+    def lower_items(self):
+        """Like iteritems(), but with all lowercase keys."""
+        return (
+            (lowerkey, keyval[1])
+            for (lowerkey, keyval)
+            in self._store.items()
+        )
+
+    def __eq__(self, other):
+        if isinstance(other, Mapping):
+            other = CaseInsensitiveDict(other)
+        else:
+            return NotImplemented
+        # Compare insensitively
+        return dict(self.lower_items()) == dict(other.lower_items())
+
+    # Copy is required
+    def copy(self):
+        return CaseInsensitiveDict(self._store.values())
+
+    def __repr__(self):
+        return str(dict(self.items()))
 
 
 def to_key_val_list(value):
@@ -98,6 +171,40 @@ def encode_params(data):
     else:
         return data
 
+
+def merge_setting(request_setting, session_setting, dict_class=OrderedDict):
+    """
+    https://github.com/psf/requests/blob/eedd67462819f8dbf8c1c32e77f9070606605231/requests/sessions.py#L50
+    Determines appropriate setting for a given request, taking into account
+    the explicit setting on that request, and the setting in the session. If a
+    setting is a dictionary, they will be merged together using `dict_class`
+    """
+
+    if session_setting is None:
+        return request_setting
+
+    if request_setting is None:
+        return session_setting
+
+    # Bypass if not a dictionary (e.g. verify)
+    if not (
+            isinstance(session_setting, Mapping) and
+            isinstance(request_setting, Mapping)
+    ):
+        return request_setting
+
+    merged_setting = dict_class(to_key_val_list(session_setting))
+    merged_setting.update(to_key_val_list(request_setting))
+
+    # Remove keys that are set to None. Extract keys first to avoid altering
+    # the dictionary during iteration.
+    none_keys = [k for (k, v) in merged_setting.items() if v is None]
+    for key in none_keys:
+        del merged_setting[key]
+
+    return merged_setting
+
+
 class Session(object):
     def __init__(self, headers=None, cert=None, verify=False, verbose=False):
         self.curl = pycurl.Curl()
@@ -113,8 +220,8 @@ class Session(object):
 
     
     def request(self, method, url, headers=None, params=None, data=None, verbose=None):
-        if headers is None:
-            headers = self.headers #TODO
+        headers=merge_setting(headers, self.headers, dict_class=CaseInsensitiveDict),
+
         if verbose is None:
             verbose = self.verbose
 
